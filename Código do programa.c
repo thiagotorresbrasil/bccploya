8af2a3f1c073fc2efa01e6c8348f90452440e3e6
thiagotorresbrasil <thiihago@gmail.com>
2016-04-06 16:54:25 -0300

    Added files via upload

---
+++ Código do programa.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<math.h>
+int main(){
+   int a;
+   float b;
+   printf("Digite o valor: ");
+   scanf("%d",&a);
+   b = sqrt(a);
+   printf("A raiz de %d = %.2f",a,b);
+   getchar();
+  getchar();
+}
